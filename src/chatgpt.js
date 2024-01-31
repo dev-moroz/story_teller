8e9280ea0197114e903d60b34969a03646590492
Anatoly Morozov <dev_moroz@mail.ru>
2024-01-31 12:46:28 +0100

    feat: change to search chat gpt

--- src/chatgpt.js
+++ src/chatgpt.js
@@ -12,31 +12,14 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
-const getMessage = m => 
-  `Напиши на основе этих тезисов последовательную, эмоциональнцю историю: ${m}
-  
-   Это тезисы c описанием ключевых моментов дня.
-   Необходимо в итоге получить такю историю, что бы я заплмнил этот день и 
-   смог в последствии рассказать ее друзьям. Много текста не нужно, главное,
-   чтобы были эмоции, правильная последовательность + учтение контекста. Длинна текста не более 3 - 4 предложений`
-
-
 export async function chatGPT(message = '') {
-  const messages = [{
-    role: ROLES.SYSTEM,
-    content: 'Ты опытный копирайтер, который пишет краткие эмоциональные статьи для соц. сетей.',
-  }, {
-    role: ROLES.USER, content: getMessage(message)
-  }]
   try {
-    const сompletion = await openai.chat.completions.create({
-      messages,
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: ROLES.USER, content: message }],
       model: CHATGPT_MODEL,
-    })
+    });
 
-    console.log("🚀 ~ сompletion:", сompletion.choices[0].message)
-    return сompletion.choices[0].message
-    // .content
+    return chatCompletion.choices[0].message
 
   } catch (e){
     console.error('Error whit send ...')
